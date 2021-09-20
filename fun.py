11828464994ff7f2873c3540b0b474671b34f54d
archiso7 <AwesomeLAX7@gmail.com>
2021-09-20 13:23:32 -0400

    fixed the snake gen

--- fun.py
+++ fun.py
@@ -16,6 +16,7 @@ def foreground():
 
 # generate the box for the game to be played in
 def game_space():
+    screen = [[' ' for i in range(width-2)] for i in range(height-2)]
     top_line = ['\u2500' for i in range(width-2)]
     bottom_line = ['\u2500' for i in range(width-2)]
     main_line = [' ' for i in range(width-2)]
@@ -24,28 +25,30 @@ def game_space():
     main_line.insert(0, '\u2502')   #
     top_line.append('\u2510')    #
     bottom_line.append('\u2518') # add the last character to each line
-    main_line.append('\u2502')   #
-    screen = [main_line for i in range(height-2)]
+    for i in screen:
+        i.insert(0, '\u2502')
+        i.append('\u2502')
     screen.insert(0, top_line) # add the top line to the start of screen
     screen.append(bottom_line) # add the bottom line to the screen
-    print(screen)
-    gen_snake()
-    display()
+    display(gen_snake(screen))
 
 # create the snake on top of the game space
-def gen_snake():
-    screen[1][1] = '\033[0;32;42m ' # highlight the character at 9,29W in the screen array
-    screen[1][1] = '\033[0;37;40m ' # make sure the lines after it are not highlighted
+def gen_snake(screen):
+    print(screen)
+    screen[round(height/2)][round(width/2)] = '\033[0;32;42m ' # highlight the character at 9,29W in the screen array
+    screen[round(height/2)][round(width/2)+1] = '\033[0;37;40m ' # make sure the lines after it are not highlighted
+    return(screen)
+
+
 
 # print everything
-def display():
+def display(screen):
     ts = ''
-    for i in screen: # loop through every row in the array
-        for s in i:  # loop through all charatcers in the array
-            ts += s
+    for i in screen:
+        for n in i:
+            ts += n
         ts += '\n'
     print(ts)
-
 # what to do when a key is pressed
 def on_press(key):
-    pass
+    exit()
--- vars.py
+++ vars.py
@@ -1,8 +1,4 @@
 # deffine all the vars
 width = 60 # set the width of the game space
 height = 20 # set the height of the game space
-screen = [] # this is an array of waht the screen should be
 snake_len = 2 # current length of the snake
-top_line = [] # list of the top line of the game space
-bottom_line = [] # list of the bottom line of the game space
-main_line = [] # list of the midle lines of the game space
